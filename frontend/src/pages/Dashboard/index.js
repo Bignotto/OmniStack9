32b0b83a21abe4880f06f3e6305571fae971780c
Thiago Bignotto <bignotto@gmail.com>
2019-10-05 10:02:41 -0300

    frontend1: form para criar spots

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,15 +1,20 @@
-import React, {useEffect} from 'react';
+import React, {useEffect,useState} from 'react';
+import {Link} from 'react-router-dom';
 import api from '../../services/api';
+import './styles.css'
 
 export default function Dashboard() {
+
+    const [spots,setSpots] = useState([]);
+
     useEffect(() => {
         async function loadSpots() {
             const user_id = localStorage.getItem('user');
             const response = await api.get('/dashboard', {
                 headers: {user_id}
             });
-
             console.log(response.data);
+            setSpots(response.data);
         }
         loadSpots();
     }, []);
@@ -17,5 +22,20 @@ export default function Dashboard() {
 
     //TODO: 1:01:50 aula 3
     //criar o loop para exibir os spots!
-    return <h1>Funciona!</h1>;
+    return (
+        <>
+            <ul className="spot-list">
+                {spots.map(spot => (
+                    <li key={spot._id}>
+                        <header style={{ backgroundImage: `url(${spot.thumbnail_url})`}} />
+                        <strong>{spot.company}</strong>
+                        <span>{spot.price ? `R$ ${spot.price}/dia` : 'Grátis!'}</span>
+                    </li>
+                ))}
+            </ul>
+            <Link to='/new'>
+                <button className="btn">Cadastrar novo Spot</button>
+            </Link>
+        </>
+    );
 }
--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,5 +1,60 @@
-import React from 'react';
+import React, {useState,useEffect,useMemo} from 'react';
+import camera from '../../assets/camera.svg';
+import './styles.css';
 
 export default function New() {
-    
+    const [company,setCompany] = useState('');
+    const [techs,setTechs] = useState('');
+    const [price,setPrice] = useState('');
+    const [thumbnail,setThumbnail] = useState(null);
+
+    const preview = useMemo(
+        () => {
+            return thumbnail ? URL.createObjectURL(thumbnail) : null
+        },
+        [thumbnail]
+    )
+
+    function handleSubmit() {
+
+    }
+
+    return(
+        <>
+            <form onSubmit={handleSubmit}>
+                <label 
+                    id="thumbnail"
+                    style={{ backgroundImage: `url(${preview})` }}
+                    className={thumbnail ? 'has-thumbnail' : ''}
+                >
+                    <input type="file" onChange={event => setThumbnail(event.target.files[0])}/>
+                    <img src={camera} alt="choose image"/>
+                </label>
+                <label htmlFor="company">Empresa *</label>
+                <input 
+                    id="company"
+                    placeholder="Sua empresa incrível"
+                    value={company}
+                    onChange={event => setCompany(event.target.value)}>
+                </input>
+                <label htmlFor="techs">Tecnologias * <span>(separado por vírgula)</span></label>
+                <input 
+                    id="techs"
+                    placeholder="Suas tecnologias"
+                    value={techs}
+                    onChange={event => setTechs(event.target.value)}>
+                </input>
+                <label htmlFor="price">Valor da Diária * <span>(em branco para gratuíto)</span></label>
+                <input 
+                    id="price"
+                    placeholder="em R$"
+                    value={price}
+                    onChange={event => setPrice(event.target.value)}>
+                </input>
+                <button className="btn">
+                    Cadastrar
+                </button>
+            </form>
+        </>
+    );
 }
